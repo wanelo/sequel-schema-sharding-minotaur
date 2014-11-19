d980ce358a80e9023406344531c3793da9f60ecb
Eric Saxby & Greg Poirier <eric+greg@wanelo.com>
2014-11-19 14:56:04 -0800

    Include http options to net/http to allow ssl to work
    Circonus http trap link to the IP address of the enterprise
    broker, so cert validation is not going to work

--- lib/reporter/writers/circonus.rb
+++ lib/reporter/writers/circonus.rb
@@ -5,15 +5,22 @@ module Reporter
     class Circonus
       attr_reader :uri
 
-      ::OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-
       def initialize(url)
         @uri = URI(url)
       end
 
       def write(body)
-        Net::HTTP.start(uri.hostname, uri.port) do |http|
+        Net::HTTP.start(uri.hostname, uri.port, http_options) do |http|
+          http.use_ssl = true if uri.scheme == 'https'
           http.request_post(uri.path, body)
+        end
+      rescue Zlib::BufError
+      end
+
+      def http_options
+        {}.tap do |options|
+          options[:use_ssl] = uri.scheme == 'https'
+          options[:verify_mode] = OpenSSL::SSL::VERIFY_NONE
         end
       end
     end
