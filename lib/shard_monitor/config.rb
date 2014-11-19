d027ca8183ecd448b1a97e5fe1e2c83148891982
Eric Saxby & Greg Poirier <eric+greg@wanelo.com>
2014-11-19 11:09:54 -0800

    Remove redundant config method

--- lib/shard_monitor/config.rb
+++ lib/shard_monitor/config.rb
@@ -7,9 +7,5 @@ module ShardMonitor
       :long  => '--redis REDIS_URI',
       :description => 'URI with which to connect to redis, eg: redis://1.1.1.1/1',
       :default => 'redis://127.0.0.1/1'
-
-    def redis
-      config[:redis]
-    end
   end
 end
