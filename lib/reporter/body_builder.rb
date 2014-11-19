e4e4a3259afcc89f456e574c9dea396ba05919cd
Eric Saxby & Greg Poirier <eric+greg@wanelo.com>
2014-11-19 14:30:11 -0800

    Send circonus payload as json

--- lib/reporter/body_builder.rb
+++ lib/reporter/body_builder.rb
@@ -1,6 +1,12 @@
+require 'oj'
+
 module Reporter
   class BodyBuilder < Struct.new(:shards)
     def lift
+      Oj.dump(body)
+    end
+
+    def body
       {
         'key' => {
           '_type' => 'n',
