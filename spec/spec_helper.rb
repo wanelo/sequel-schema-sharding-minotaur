1a2ed00f177a63055f3d4b12f1568a7d6da3722d
Eric Saxby & Greg Poirier <eric+greg@wanelo.com>
2014-11-19 11:09:28 -0800

    Reset global configuration before each test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+Bundler.require :test, :development
+
+require 'reporter'
+require 'shard_monitor'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -5,5 +10,14 @@ RSpec.configure do |config|
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
+  end
+
+  config.after :each do
+    [Reporter, ShardMonitor].each do |klass|
+      [:@redis, :@config].each do |var|
+        next unless klass.instance_variable_get(var)
+        klass.send(:remove_instance_variable, var)
+      end
+    end
   end
 end
