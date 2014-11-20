cfe40a108644aa14590663cb7e9e414b430cd2cb
Eric Saxby & Greg Poirier <eric+greg@wanelo.com>
2014-11-19 16:29:57 -0800

    shard monitor requires —table option

--- lib/shard_monitor/config.rb
+++ lib/shard_monitor/config.rb
@@ -7,5 +7,11 @@ module ShardMonitor
       :long  => '--redis REDIS_URI',
       :description => 'URI with which to connect to redis, eg: redis://1.1.1.1/1',
       :default => 'redis://127.0.0.1/1'
+
+    option :table_name,
+      :short => '-t TABLE',
+      :long  => '--table TABLE',
+      :description => 'Name of table used by a sequel-schema-sharding model',
+      :required => true
   end
 end
